e859ab50148bf91834e7545ce3e9434958378880
Brunno <bbsouza957@gmail.com>
2024-03-13 09:17:15 -0300

    sprint1

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def calculadora(num1, num2, operador):
+    match operador:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case _:
+            return "Operador inválido"        
