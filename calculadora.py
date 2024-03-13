162b4a3a6d9cf17f1d665715262e7c6c654f44c4
Brunno <bbsouza957@gmail.com>
2024-03-13 09:18:42 -0300

    sprint3

--- calculadora.py
+++ calculadora.py
@@ -11,3 +11,10 @@ def calculadora(num1, num2, operador):
         case _:
             return "Operador inválido"        
 
+operador = input("Insira o operador (+, -, *, /), 0 para encerrar: ")
+while operador != "0":
+    num1 = float(input("Insira o primeiro número: "))
+    num2 = float(input("Insira o segundo número: "))
+    resultado = calculadora(num1, num2, operador)
+    print(f"Resultado: {resultado}")
+    operador = input("Insira o operador (+, -, *, /), 0 para encerrar: ")
